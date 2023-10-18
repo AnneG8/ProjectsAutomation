119306e924f6f0ef2e85fec5d01d3c2de113f793
Anna Veselova <anna.veselova.97@mail.ru>
2023-10-18 15:03:05 +0300

    fix TabError

--- bot/utils.py
+++ bot/utils.py
@@ -5,8 +5,8 @@ from ProjectsAutomation.settings import CALL_DURATION
 # что делать, если кол-во часов перевалило за 24?
 def get_end_time(start_time: time):
     new_minutes = start_time.minute + CALL_DURATION.seconds // 60
-	new_hours = start_time.hour + new_minutes // 60
-	return time(int(new_hours % 24), int(new_minutes % 60))
+    new_hours = start_time.hour + new_minutes // 60
+    return time(int(new_hours % 24), int(new_minutes % 60))
 
 
 # Добавить функцию перерасчета на дальневосточное время (+7ч от МСК)

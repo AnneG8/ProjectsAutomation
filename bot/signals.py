aa5d5fb9b7237f0b4a4ea0c4f0d8d4de20cb0c2e
Anna Veselova <anna.veselova.97@mail.ru>
2023-10-21 20:22:12 +0300

    update json parsing

--- bot/signals.py
+++ bot/signals.py
@@ -1,7 +1,7 @@
 import json
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from datetime import datetime
+from datetime.datetime import strptime
 
 from bot.models import Project, ProjectManager
 from bot.management.commands.bot import choose_time_calling
@@ -14,21 +14,22 @@ def parse_project_files(sender, instance, **kwargs):
             pms = json.load(file)
 
             for pm in pms:
-                time_from = datetime.strptime(
-                    pm.get('time_from'), 
-                    "%H:%M"
-                ).time()
-                time_to = datetime.strptime(
-                    pm.get('time_to'), 
-                    "%H:%M"
-                ).time()
+                id_telegram = pm.get('id_telegram')
+                name = pm.get('name')
+                if not id_telegram or not name:
+                    continue
+
+                time_from = strptime(pm.get('time_from'), "%H:%M").time()
+                time_to = strptime(pm.get('time_to'), "%H:%M").time()
+                is_active = True if time_from and time_to else False
 
                 ProjectManager.objects.update_or_create(
-                    id_telegram=pm.get('id_telegram'),
+                    id_telegram=id_telegram,
                     defaults={
-                        'name': pm.get('name'),
+                        'name': name,
                         'time_from': time_from,
-                        'time_to': time_to
+                        'time_to': time_to,
+                        'is_active': is_active
                     }
                 )
         # распарсить и students_file тоже

48efb7ef189a8ed7868399df148ec3c94eee6d2f
Anna Veselova <anna.veselova.97@mail.ru>
2023-10-21 21:52:37 +0300

    add student file parsing

--- bot/signals.py
+++ bot/signals.py
@@ -1,37 +1,69 @@
 import json
 from django.db.models.signals import post_save
+from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 from datetime.datetime import strptime
 
-from bot.models import Project, ProjectManager
+from bot.models import Project, ProjectManager, Student, StudyLevel
 from bot.management.commands.bot import choose_time_calling
+
+
+def pm_parsing(pm):
+    id_telegram = pm.get('id_telegram')
+    name = pm.get('name')
+    if not id_telegram or not name:
+        continue
+
+    time_from = strptime(pm.get('time_from'), "%H:%M").time()
+    time_to = strptime(pm.get('time_to'), "%H:%M").time()
+    is_active = True if time_from and time_to else False
+
+    ProjectManager.objects.update_or_create(
+        id_telegram=id_telegram,
+        defaults={
+            'name': name,
+            'time_from': time_from,
+            'time_to': time_to,
+            'is_active': is_active
+        }
+    )   
+
+
+def student_parsing(student):
+    id_telegram = student.get('id_telegram')
+    name = student.get('name')
+    level_id = student.get('level_id')
+    if not id_telegram or not name or not level_id:
+        continue
+    try:
+        level = get_object_or_404(StudyLevel, id=int(level_id))
+        Student.objects.update_or_create(
+            id_telegram=id_telegram,
+            defaults={
+                'name': name,
+                'level': level,
+                'is_active': True
+            }
+        )
+    except Http404:
+        continue
+    
 
 
 @receiver(post_save, sender=Project)
 def parse_project_files(sender, instance, **kwargs):
+    print("parse_project_files запустился")
     if instance.pm_file:
         with open(instance.pm_file.path, 'r') as file:
             pms = json.load(file)
-
             for pm in pms:
-                id_telegram = pm.get('id_telegram')
-                name = pm.get('name')
-                if not id_telegram or not name:
-                    continue
+                pm_parsing(pm)
 
-                time_from = strptime(pm.get('time_from'), "%H:%M").time()
-                time_to = strptime(pm.get('time_to'), "%H:%M").time()
-                is_active = True if time_from and time_to else False
+        if instance.students_file:
+            with open(instance.students_file.path, 'r') as file:
+                students = json.load(file)
+                for student in students:
+                    student_parsing(student)
 
-                ProjectManager.objects.update_or_create(
-                    id_telegram=id_telegram,
-                    defaults={
-                        'name': name,
-                        'time_from': time_from,
-                        'time_to': time_to,
-                        'is_active': is_active
-                    }
-                )
-        # распарсить и students_file тоже
         # удалять распарсенные файлы?
         choose_time_calling()
